c48bd43f8416b0f4a9b61b94159775c788b242e7
JMIDEV <josemiguelpereztejero@gmail.com>
2023-03-04 17:14:16 +0100

    First upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1677946111,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/titletext-sheet0.png",
+		"images/backgroundtitle-sheet0.png",
+		"images/explanationbutton-sheet0.png",
+		"images/huecobutton-sheet0.png",
+		"images/huecobutton-sheet1.png",
+		"images/transition-sheet0.png",
+		"images/blueandredbgtitlescreen-sheet0.png",
+		"images/explanationcontent-sheet0.png",
+		"images/explanationcontent-sheet1.png",
+		"images/continuebutton-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
